5c711f96f5718046b4c7656e14ca43deccf84484
Christian Machira <christianmachira8@gmail.com>
2024-03-20 15:53:57 +0300

    mini-project submission

---
+++ Dictionary/mini_project.py
@@ -0,0 +1,48 @@
+# creating a program that will allow a user to input a word and then the program will return the definition of the word.
+# The program will also return a message if the word is not found in the dictionary.
+# the program will also consider input using different cases, upper loer or mixed
+
+# importing the json module
+import json
+
+# load json file
+with open('/Users/christianmachira/Developer/Python PLP/Dictionary/data.json', 'r') as file:
+    data = json.load(file)
+
+# create a class
+class dictionary:
+        
+        # create a method that takes a word as an argument
+        def __init__(self, word):
+            self.word = word
+
+        # create a method that will return the definition of the word
+        def get_definition(self):
+            # check if the word is in the dictionary
+            if self.word in data:
+                return data[self.word]
+            # check if the word is in the dictionary in lower case
+            elif self.word.lower() in data:
+                return data[self.word.lower()]
+            # check if the word is in the dictionary in upper case
+            elif self.word.upper() in data:
+                return data[self.word.upper()]
+            # check if the word is in the dictionary in title case
+            elif self.word.title() in data:
+                return data[self.word.title()]
+            else:
+                return "The word does not exist in the dictionary"
+
+
+
+# ask user for input
+input_word= input("Enter a word: ")
+
+# create an instance of the class
+Dictionary = dictionary(input_word)
+
+# look up the definition of the word
+meaning = Dictionary.get_definition()
+
+# print the definition
+print(meaning)
---
+++ Dictionary/readme.md
@@ -0,0 +1,5 @@
+- this a simple dictionary program in Python
+- it prompts the user to input a word, no matter the case
+- prints out the definition of the  word if found in the dictionary. If not, it says "word not found"
+
+
